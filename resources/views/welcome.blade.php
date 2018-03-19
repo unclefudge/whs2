cf5bfe3d2b847d1553180905460d2d860eab2b4f
= <=>
2018-03-19 15:11:42 +1100

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,16 +2,21 @@
 
 @section('pagetitle')
     <div class="page-title">
-        <h1>Welcome to Safework</h1>
+        <h1>Welcome to Safeworksite</h1>
     </div>
 @stop
 
 @section('content')
-    <div class="page-content-inner">
+    <div class="page-content-inner" style="background: #ffffff">
         <div class="row">
             <div class="col-md-12">
-                <p style="text-align:center;"><img src="/img/safety-gear.jpg" class="img-responsive" align="middle"></p>
+                <img src="/img/logo-sws-large.png" class="img-responsive" align="middle" style="margin: 0 auto;">
             </div>
+            <div class="col-md-12">
+                <video class="img-responsive" src="/img/sws-video.mp4" controls>
+            </div>
+
+
         </div>
 
         <div class="row">
