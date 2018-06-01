171a26dd5ba51cbcce7adca8c2a4859650d03f55
= <=>
2018-06-01 16:27:35 +1000

    update

--- resources/views/company/_edit-construction.blade.php
+++ resources/views/company/_edit-construction.blade.php
@@ -19,52 +19,54 @@
             </div>
         </div>
         <hr class="field-hr">
-        {{-- Planner Name --}}
-        <div class="row">
-            <div class="form-group {!! fieldHasError('nickname', $errors) !!}">
-                {!! Form::label('nickname', 'Planner Name:', ['class' => 'col-md-3 control-label']) !!}
-                <div class="col-md-9">
-                    {!! Form::text('nickname', $company->nickname, ['class' => 'form-control']) !!}
-                    {!! fieldErrorMessage('nickname', $errors) !!}
-                </div>
-            </div>
-        </div>
-        <hr class="field-hr">
-        {{-- Max Jobs --}}
-        <div class="row">
-            <div class="form-group {!! fieldHasError('maxjobs', $errors) !!}">
-                {!! Form::label('maxjobs', 'Max Jobs:', ['class' => 'col-md-3 control-label']) !!}
-                <div class="col-md-9">
-                    {!! Form::text('maxjobs', $company->maxjobs, ['class' => 'form-control', 'required']) !!}
-                    {!! fieldErrorMessage('maxjobs', $errors) !!}
-                </div>
-            </div>
-        </div>
-        <hr class="field-hr">
-
-        {{-- Transient --}}
-        <div class="row">
-            <div class="form-group {!! fieldHasError('transient', $errors) !!}">
-                {!! Form::label('transient', 'Transient:', ['class' => 'col-md-3 control-label']) !!}
-                <div class="col-md-9">
-                    {!! Form::select('transient', ['0' => 'No', '1' => 'Yes'], $company->transient, ['class' => 'form-control bs-select']) !!}
-                    {!! fieldErrorMessage('transient', $errors) !!}
-                </div>
-            </div>
-        </div>
-
-        <div id="super-div" @if (!$company->transient) style="display: none" @endif>
-            <hr class="field-hr">
+        @if(Auth::user()->isCC())
+            {{-- Planner Name --}}
             <div class="row">
-                <div class="form-group {!! fieldHasError('supervisors', $errors) !!}">
-                    {!! Form::label('supervisors', 'Supervisor:', ['class' => 'col-md-3 control-label']) !!}
+                <div class="form-group {!! fieldHasError('nickname', $errors) !!}">
+                    {!! Form::label('nickname', 'Planner Name:', ['class' => 'col-md-3 control-label']) !!}
                     <div class="col-md-9">
-                        {!! Form::select('supervisors', Auth::user()->company->supervisorsSelect(),  $company->supervisedBy->pluck('id')->toArray(), ['class' => 'form-control select2', 'name' => 'supervisors[]', 'title' => 'Select one or more trades', 'multiple', 'id' => 'supervisors']) !!}
-                        {!! fieldErrorMessage('supervisors', $errors) !!}
+                        {!! Form::text('nickname', $company->nickname, ['class' => 'form-control']) !!}
+                        {!! fieldErrorMessage('nickname', $errors) !!}
                     </div>
                 </div>
             </div>
-        </div>
+            <hr class="field-hr">
+            {{-- Max Jobs --}}
+            <div class="row">
+                <div class="form-group {!! fieldHasError('maxjobs', $errors) !!}">
+                    {!! Form::label('maxjobs', 'Max Jobs:', ['class' => 'col-md-3 control-label']) !!}
+                    <div class="col-md-9">
+                        {!! Form::text('maxjobs', $company->maxjobs, ['class' => 'form-control', 'required']) !!}
+                        {!! fieldErrorMessage('maxjobs', $errors) !!}
+                    </div>
+                </div>
+            </div>
+            <hr class="field-hr">
+
+            {{-- Transient --}}
+            <div class="row">
+                <div class="form-group {!! fieldHasError('transient', $errors) !!}">
+                    {!! Form::label('transient', 'Transient:', ['class' => 'col-md-3 control-label']) !!}
+                    <div class="col-md-9">
+                        {!! Form::select('transient', ['0' => 'No', '1' => 'Yes'], $company->transient, ['class' => 'form-control bs-select']) !!}
+                        {!! fieldErrorMessage('transient', $errors) !!}
+                    </div>
+                </div>
+            </div>
+
+            <div id="super-div" @if (!$company->transient) style="display: none" @endif>
+                <hr class="field-hr">
+                <div class="row">
+                    <div class="form-group {!! fieldHasError('supervisors', $errors) !!}">
+                        {!! Form::label('supervisors', 'Supervisor:', ['class' => 'col-md-3 control-label']) !!}
+                        <div class="col-md-9">
+                            {!! Form::select('supervisors', Auth::user()->company->supervisorsSelect(),  $company->supervisedBy->pluck('id')->toArray(), ['class' => 'form-control select2', 'name' => 'supervisors[]', 'title' => 'Select one or more trades', 'multiple', 'id' => 'supervisors']) !!}
+                            {!! fieldErrorMessage('supervisors', $errors) !!}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endif
 
         <br>
         <div class="form-actions right">
