616d72839cefa6819911320ef663bcdd7421bcf8
Fudge Jordan <fudge@jordan.net.au>
2018-11-18 15:49:54 +1100

    update

--- resources/views/misc/equipment/transfer.blade.php
+++ resources/views/misc/equipment/transfer.blade.php
@@ -22,10 +22,6 @@
                             <span class="caption-subject font-green-haze bold uppercase">Transfer Item </span>
                             <span class="caption-helper"> - ID: {{ $location->id }}</span>
                         </div>
-                        <div class="actions">
-                            <a href="" class="btn btn-circle btn-icon-only btn-default collapse"> </a>
-                            <a href="javascript:;" class="btn btn-circle btn-icon-only btn-default fullscreen"> </a>
-                        </div>
                     </div>
                     <div class="portlet-body form">
                         <!-- BEGIN FORM-->
