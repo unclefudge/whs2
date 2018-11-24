c06faa87b6605d3b2e0c0cacc217c7c0cde1547f
Fudge Jordan <fudge@jordan.net.au>
2018-11-24 15:19:33 +1100

    update

--- resources/views/misc/equipment/show.blade.php
+++ resources/views/misc/equipment/show.blade.php
@@ -33,7 +33,8 @@
                                 </div>
                                 <div class="col-md-5">
                                     @if (!$equip->status)
-                                    <h2 style="margin-top: 0px" class="font-red">DELETED {{ $equip->updated_at->format('d/m/Y)') }}</h2>
+                                        <h2 style="margin-top: 0px" class="font-red">DELETED {{ $equip->updated_at->format('d/m/Y)') }}</h2>
+                                    @endif
                                 </div>
                             </div>
 
