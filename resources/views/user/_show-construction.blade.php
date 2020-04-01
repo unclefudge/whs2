9fb94ffe18c71e5f68a246e8652c6f98a1853591
Fudge Jordan <fudge@jordan.net.au>
2020-04-01 11:12:45 +1100

    update

--- resources/views/user/_show-construction.blade.php
+++ resources/views/user/_show-construction.blade.php
@@ -5,7 +5,7 @@
             <span class="caption-subject font-dark bold uppercase">Construction</span>
         </div>
         <div class="actions">
-            @if (Auth::user()->allowed2('edit.company.con', $user) && $user->status)
+            @if (Auth::user()->allowed2('edit.user.construction', $user) && $user->status)
                 <button class="btn btn-circle green btn-outline btn-sm" onclick="editForm('construction')">Edit</button>
             @endif
         </div>
